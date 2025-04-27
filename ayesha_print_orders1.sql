edec98f47b35eddbb74d03d5ac7e4213275939ac
Ashraf Ali Khan <103625515+ashrafalifire@users.noreply.github.com>
2025-04-27 18:34:31 +0100

    Add files via upload

---
+++ ayesha_print_orders1.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ayesha
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `print_orders1`
+--
+
+DROP TABLE IF EXISTS `print_orders1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `print_orders1` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `department` varchar(100) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `copies` int DEFAULT NULL,
+  `print_type` varchar(50) DEFAULT NULL,
+  `payment_mode` varchar(20) DEFAULT NULL,
+  `payment_proof_path` varchar(255) DEFAULT NULL,
+  `detials` varchar(255) DEFAULT NULL,
+  `file_upload_path` blob,
+  `submission_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `print_orders1`
+--
+
+LOCK TABLES `print_orders1` WRITE;
+/*!40000 ALTER TABLE `print_orders1` DISABLE KEYS */;
+INSERT INTO `print_orders1` VALUES (1,'AYESHA KHANUM.R','220071601044@crescent.education','cse -A',3,'8639701022',1,'Black and White','Cash','','print single sde',_binary 'E:\\ayeshubuisness\\shafaa-prints\\src\\uploads\\1745461731539-OUTPUT OF AST.pdf','2025-04-24 02:28:51'),(2,'AYESHA KHANUM.R','220071601044@crescent.education','cse -A',3,'8639701022',1,'Black and White','Cash','','print single sde',_binary 'E:\\ayeshubuisness\\shafaa-prints\\src\\uploads\\1745519087994-OUTPUT OF AST.pdf','2025-04-24 18:24:48');
+/*!40000 ALTER TABLE `print_orders1` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-27 22:59:36
